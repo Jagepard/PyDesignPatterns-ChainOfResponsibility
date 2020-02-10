e740eec1f56abdfa5860877dec3cea2777590e99
Jagepard <dankorot@gmail.com>
2020-02-10 18:30:22 +0300

    deleted

--- handler.py
+++
@@ -1,6 +0,0 @@
-class Handler:
-    def getClassName(self):
-        return __name__
-    
-    def execute(self):
-        print(__name__)
--- notice.py
+++
@@ -1,8 +0,0 @@
-from handler import Handler
-
-class Notice(Handler):
-    def getClassName(self):
-        return __name__
-
-    def execute(self):
-        print(__name__)
